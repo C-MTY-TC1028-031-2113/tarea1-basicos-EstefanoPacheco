23a738b87a4e37503904469a056bcc3d13f9b5f8
EstefanoPacheco <a00835025@tec.com>
2021-08-20 18:22:54 -0500

    20082021

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    mat1 = float(input("Ingresa la calificación de la primera materia: "))
+    mat2 = float(input("Ingresa la calificación de la sgunda materia: "))
+    mat3 = float(input("Ingresa la calificación de la tercera materia: "))
+    mat4 = float(input("Ingresa la calificación de la cuarta materia: "))
+    promedio = (mat1 + mat2 + mat3 + mat4)/4
+    print("El promedio de tus calificaciones es: " + str(promedio))
 
 
 if __name__ == '__main__':
